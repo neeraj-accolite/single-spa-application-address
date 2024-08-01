2008a1e76e3f5230442d394bee268e05fad8dcd4
Neeraj Agrawal <neeraj.agrawal@accolitedigital.com>
2024-08-01 22:08:00 +0530

    code refactored

--- src/acc-address.ts
+++ src/acc-address.ts
@@ -2,7 +2,6 @@ import { h, createApp } from 'vue';
 import singleSpaVue from 'single-spa-vue';
 import App from './App.vue';
 
-// export let mountParcel: any;
 const appOptions = {
     render() {
         return h(App);
@@ -12,19 +11,5 @@ const vueLifecycles = singleSpaVue({
     createApp,
     appOptions,
 });
-
-
-// export const bootstrap = [
-//     (props: any) => {
-//         mountParcel = props.mountParcel
-//         return Promise.resolve()
-//     }
-// ];
-// export const mount = [
-//     vueLifecycles.mount
-// ];
-// export const unmount = [
-//     vueLifecycles.unmount
-// ];
 
 export const { bootstrap, mount, unmount } = vueLifecycles;
